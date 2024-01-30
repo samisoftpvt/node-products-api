5dba9417239b08ee6f55ba12a977e0e3396eb3c4
sami3114 <sami.softpvt@gmail.com>
2024-01-31 00:08:26 +0500

    fest: middleware and validations

--- app/routes/web.js
+++ app/routes/web.js
@@ -1,8 +1,10 @@
 const EmployeeController=require('../controllers/EmployeeController');
+const validateRequest=require('../middleware/validateRequest');
+const employeeValidator=require('../validators/employeeValidator');
 
 module.exports=function (app){
     app.get('/employees',EmployeeController.index);
-    app.post('/employees',EmployeeController.store);
-    app.put('/employees/:id',EmployeeController.update);
+    app.post('/employees',employeeValidator.createEmployeeValidator,validateRequest,EmployeeController.store);
+    app.put('/employees/:id',employeeValidator.createEmployeeValidator,validateRequest,EmployeeController.update);
     app.delete('/employees/:id',EmployeeController.delete);
 }
---
+++ app/validators/employeeValidator.js
@@ -0,0 +1,7 @@
+const { body }=require('express-validator');
+
+exports.createEmployeeValidator = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('phone').notEmpty().withMessage('Phone is required'),
+];
